2d3f514fa728ab0bce3d20becdb3fb3222f38b2e
Marcio Segurado <marcio.pereira@mpsolucoes.com.br>
2015-11-24 10:44:32 -0200

    Publicacao

---
+++ Content/plugin/js/app.js
@@ -0,0 +1,41 @@
+﻿function msgbox(mensagem) {
+    $('#conteudoMensagem').html(mensagem);
+    $('#alertModal').modal('show');
+}
+
+function checkDate(field) {
+    
+    var allowBlank = true;
+    var minYear = 1902;
+    //var maxYear = (new Date()).getFullYear();
+    var maxYear = 2060;
+
+    var errorMsg = "";
+
+    // regular expression to match required date format
+    re = /^(\d{1,2})\/(\d{1,2})\/(\d{4})$/;
+
+    if ($('#'+field).val() != '') {
+        if (regs = $('#' + field).val().match(re)) {
+            if (regs[1] < 1 || regs[1] > 31) {
+                errorMsg = "Valor inválido para o dia: " + regs[1];
+            } else if (regs[2] < 1 || regs[2] > 12) {
+                errorMsg = "Valor inválido para o mês: " + regs[2];
+            } else if (regs[3] < minYear || regs[3] > maxYear) {
+                errorMsg = "Valor Inválido para o ano: " + regs[3] + " - deve estar entre " + minYear + " e " + maxYear;
+            }
+        } else {
+            errorMsg = "Formato inválido de dado: " + field.value;
+        }
+    } else if (!allowBlank) {
+        errorMsg = "Datas vazias não são permitidas!";
+    }
+
+    if (errorMsg != "") {
+        msgbox(errorMsg);
+        $('#' + field).focus();
+        return false;
+    }
+
+    return true;
+}
---
+++ ListaHospedePage.aspx
@@ -0,0 +1,21 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="ListaHospedePage.aspx.cs" Inherits="GerenciamentoHotel.ListaHospedePage" %>
+
+<%@ Register assembly="Microsoft.ReportViewer.WebForms, Version=11.0.0.0, Culture=neutral, PublicKeyToken=89845dcd8080cc91" namespace="Microsoft.Reporting.WebForms" tagprefix="rsweb" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+    <body>
+        <form id="form1" runat="server">
+            <div align="center">
+                <asp:ScriptManager ID="ScriptManager1" runat="server" EnablePageMethods="true" EnablePartialRendering="False" ></asp:ScriptManager>
+                <rsweb:ReportViewer  ID="relatorio" runat="server" Font-Names="Verdana" Font-Size="8pt" WaitMessageFont-Names="Verdana" WaitMessageFont-Size="14pt" Width="100%" Height="700px" ShowPrintButton="True">
+                </rsweb:ReportViewer>
+                <asp:ObjectDataSource ID="ObjectDataSource1" runat="server" SelectMethod="Clients" TypeName="rpData"></asp:ObjectDataSource>
+            </div>
+        </form>
+    </body>
+</html>
